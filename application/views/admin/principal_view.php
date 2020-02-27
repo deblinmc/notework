1aa46d154bf7360deb21f4da76061a28bce2721e
deblinmc <deblinmc@outlook.com>
2020-02-26 21:40:03 -0400

    adicionado controlador y vista principal Trabajo

---
+++ application/views/admin/principal_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRINCIPA</title>
+</head>
+<body>
+<div class="">
+	Mostrando Informacion Principal de los Trabajo y Actividades:
+	Nombre: Actividad : Fecha : Estado : Anotacion
+	
+</div>
+</body>
+</html>
